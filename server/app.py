6b6476b9019de810b122b013e0e0f1463b178260
+ <kibetg984@gmail.com>
2025-01-19 13:24:26 +0300

    Built my first Flask app

--- server/app.py
+++ server/app.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
+# Create an instance of the Flask class
 app = Flask(__name__)
+
+# Define the route for the index page
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# Define a route with a variable username
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# Run the app if this script is executed directly
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
